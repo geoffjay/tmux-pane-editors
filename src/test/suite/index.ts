2c02bffef9673bfe4d98df587e3099d99fbac148
Geoff Johnson <geoff.jay@gmail.com>
2025-05-16 15:47:51 -0700

    chore: remove unnecessasry comments

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -3,26 +3,22 @@ import Mocha from "mocha";
 import * as glob from "glob";
 
 export function run(): Promise<void> {
-  // Create the mocha test
   const mocha = new Mocha({
     ui: "tdd",
     color: true,
-    timeout: 60000 // Longer timeout for version compatibility tests
+    timeout: 60000
   });
 
   const testsRoot = path.resolve(__dirname, ".");
 
   return new Promise<void>((resolve, reject) => {
-    // Use glob.sync directly
     const testFiles = glob.glob.sync("**/**.test.js", { cwd: testsRoot });
     
-    // Add files to the test suite
     testFiles.forEach((file: string) => {
       mocha.addFile(path.resolve(testsRoot, file));
     });
 
     try {
-      // Run the mocha test
       mocha.run((failures: number) => {
         if (failures > 0) {
           reject(new Error(`${failures} tests failed.`));
